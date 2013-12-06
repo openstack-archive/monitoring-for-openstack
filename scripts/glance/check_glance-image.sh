79a2f06c6142d7b1896986cdc0ba6abcdb22f3dd
gaelL <gael.lambert@enovance.com>
2013-12-06 12:04:16 +0100

    check_glance-image.sh : Change auth_url param. Now you can give full url like http://localhost:5000/v2.0. Add also glance endpoint param.

--- scripts/glance/check_glance-image.sh
+++ scripts/glance/check_glance-image.sh
@@ -32,15 +32,16 @@ STATE_DEPENDENT=4
 usage ()
 {
     echo "Usage: $0 [OPTIONS]"
-    echo " -h               Get help"
-    echo " -H <Auth URL>    URL for obtaining an auth token. Ex: http://localhost"
-    echo " -T <tenant>      Tenant to use to get an auth token"
-    echo " -U <username>    Username to use to get an auth token"
-    echo " -P <password>    Password to use ro get an auth token"
-    echo " -N <name>        Name of the monitoring image"
+    echo " -h                   Get help"
+    echo " -E <Endpoint URL>    URL for glance API. Ex: http://localhost:9292/v1"
+    echo " -H <Auth URL>        URL for obtaining an auth token. Ex: http://localhost:5000/v2.0"
+    echo " -T <tenant>          Tenant to use to get an auth token"
+    echo " -U <username>        Username to use to get an auth token"
+    echo " -P <password>        Password to use to get an auth token"
+    echo " -N <name>            Name of the monitoring image"
 }
 
-while getopts 'hH:U:T:P:N:' OPTION
+while getopts 'hH:U:T:P:N:E:' OPTION
 do
     case $OPTION in
         h)
@@ -49,6 +50,9 @@ do
             ;;
         H)
             export OS_AUTH_URL=$OPTARG
+            ;;
+        E)
+            export ENDPOINT_URL=$OPTARG
             ;;
         T)
             export OS_TENANT=$OPTARG
@@ -69,6 +73,10 @@ do
     esac
 done
 
+# Set default values
+OS_AUTH_URL=${OS_AUTH_URL:-"http://localhost:5000/v2.0"}
+ENDPOINT_URL=${ENDPOINT_URL:-"http://localhost:9292/v1"}
+
 if ! which curl >/dev/null 2>&1
 then
     echo "curl is not installed."
@@ -76,13 +84,13 @@ then
 fi
 
 # Get a token from Keystone
-TOKEN=$(curl -s -X 'POST' ${OS_AUTH_URL}:5000/v2.0/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'" ,"tenant":"'$OS_TENANT'"}}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TOKEN=$(curl -s -X 'POST' ${OS_AUTH_URL}/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'" ,"tenant":"'$OS_TENANT'"}}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 # Use the token to get a tenant ID. By default, it takes the second tenant
-TENANT_ID=$(curl -s -H "X-Auth-Token: $TOKEN" ${OS_AUTH_URL}:5000/v2.0/tenants |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep id|awk 'NR==1'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TENANT_ID=$(curl -s -H "X-Auth-Token: $TOKEN" ${OS_AUTH_URL}/tenants |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep id|awk 'NR==1'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 # Get a second tenant to avoid the 401 from Glance
-TOKEN_TENANT=$(curl -s -X 'POST' ${OS_AUTH_URL}:5000/v2.0/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'"} ,"tenantId":"'$TENANT_ID'"}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TOKEN_TENANT=$(curl -s -X 'POST' ${OS_AUTH_URL}/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'"} ,"tenantId":"'$TENANT_ID'"}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 if [ -z "$TOKEN_TENANT" ]; then
     echo "Unable to get a token from Keystone API"
@@ -95,7 +103,7 @@ START=`date +%s`
 ( dd if=/dev/zero of=/tmp/${IMAGE_NAME}.img bs=1M count=1 ) > /dev/null 2>&1
 
 # Upload the image
-IMAGE=$(curl -s -i -X POST -H "X-Auth-Token: $TOKEN_TENANT" -H "x-image-meta-container_format: bare" -H "Transfer-Encoding: chunked" -H "User-Agent: python-glanceclient" -H "Content-Type: application/octet-stream" -H "x-image-meta-disk_format: qcow2" -H "x-image-meta-name: $IMAGE_NAME" -d "<open file /tmp/${IMAGE_NAME}.img, mode 'r' at 0x7fc48f5b4150>" ${OS_AUTH_URL}:9292/v1/images)
+IMAGE=$(curl -s -i -X POST -H "X-Auth-Token: $TOKEN_TENANT" -H "x-image-meta-container_format: bare" -H "Transfer-Encoding: chunked" -H "User-Agent: python-glanceclient" -H "Content-Type: application/octet-stream" -H "x-image-meta-disk_format: qcow2" -H "x-image-meta-name: $IMAGE_NAME" -d "<open file /tmp/${IMAGE_NAME}.img, mode 'r' at 0x7fc48f5b4150>" ${ENDPOINT_URL}/images)
 
 # Get the image ID
 IMAGE_ID=$(echo $IMAGE | awk  '{ n=split($0,a,",") ; for (i=1; i<=n; i++) print a[i] }' | awk -v var="id" '$0 ~ var { print $2 }' | sed 's/"//g')
@@ -103,7 +111,7 @@ IMAGE_ID=$(echo $IMAGE | awk  '{ n=split($0,a,",") ; for (i=1; i<=n; i++) print 
 rm -f /tmp/${IMAGE_NAME}.img
 
 # Delete the image
-curl -s -X DELETE -H "X-Auth-Token: $TOKEN_TENANT" -H "Content-Type: application/octet-stream" -H "User-Agent: python-glanceclient" ${OS_AUTH_URL}:9292/v1/images/${IMAGE_ID} > /dev/null 2>&1
+curl -s -X DELETE -H "X-Auth-Token: $TOKEN_TENANT" -H "Content-Type: application/octet-stream" -H "User-Agent: python-glanceclient" ${ENDPOINT_URL}/images/${IMAGE_ID} > /dev/null 2>&1
 
 END=`date +%s`
 TIME=$((END-START))
