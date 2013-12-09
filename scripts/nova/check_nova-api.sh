3f25d4dbe3f06e041346f2f26ce8fc1c29c6c0da
gaelL <gael.lambert@enovance.com>
2013-12-09 11:12:59 +0100

    nova : Change auth_url param. Now you can give full url like http://localhost:5000/v2.0.

--- scripts/nova/check_nova-api.sh
+++ scripts/nova/check_nova-api.sh
@@ -32,14 +32,15 @@ STATE_DEPENDENT=4
 usage ()
 {
     echo "Usage: $0 [OPTIONS]"
-    echo " -h               Get help"
-    echo " -H <Auth URL>    URL for obtaining an auth token. Ex: http://localhost"
-    echo " -T <tenant>      Tenant to use to get an auth token"
-    echo " -U <username>    Username to use to get an auth token"
-    echo " -P <password>    Password to use ro get an auth token"
+    echo " -h                   Get help"
+    echo " -H <Auth URL>        URL for obtaining an auth token. Ex: http://localhost:5000/v2.0"
+    echo " -E <Endpoint URL>    URL for nova API. Ex: http://localhost:8774/v2"
+    echo " -T <tenant>          Tenant to use to get an auth token"
+    echo " -U <username>        Username to use to get an auth token"
+    echo " -P <password>        Password to use ro get an auth token"
 }
 
-while getopts 'h:H:U:T:P:' OPTION
+while getopts 'hH:U:T:P:E:' OPTION
 do
     case $OPTION in
         h)
@@ -48,6 +49,9 @@ do
             ;;
         H)
             export OS_AUTH_URL=$OPTARG
+            ;;
+        E)
+            export ENDPOINT_URL=$OPTARG
             ;;
         T)
             export OS_TENANT=$OPTARG
@@ -65,6 +69,10 @@ do
     esac
 done
 
+# Set default values
+OS_AUTH_URL=${OS_AUTH_URL:-"http://localhost:5000/v2.0"}
+ENDPOINT_URL=${ENDPOINT_URL:-"http://localhost:8774/v2"}
+
 if ! which curl >/dev/null 2>&1
 then
     echo "curl is not installed."
@@ -72,10 +80,10 @@ then
 fi
 
 # Get a token from Keystone
-TOKEN=$(curl -s -X 'POST' ${OS_AUTH_URL}:5000/v2.0/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'"}, "tenantName":"'$OS_TENANT'"}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TOKEN=$(curl -s -X 'POST' ${OS_AUTH_URL}/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'"}, "tenantName":"'$OS_TENANT'"}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 # Use the token to get a tenant ID. By default, it takes the second tenant
-TENANT_ID=$(curl -s -H "X-Auth-Token: $TOKEN" ${OS_AUTH_URL}:5000/v2.0/tenants |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep id|awk 'NR==1'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TENANT_ID=$(curl -s -H "X-Auth-Token: $TOKEN" ${OS_AUTH_URL}/tenants |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep id|awk 'NR==1'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 if [ -z "$TOKEN" ]; then
     echo "Unable to get a token from Keystone API"
@@ -83,7 +91,7 @@ if [ -z "$TOKEN" ]; then
 fi
 
 START=`date +%s`
-FLAVORS=$(curl -s -H "X-Auth-Token: $TOKEN" ${OS_AUTH_URL}:8774/v2/${TENANT_ID}/flavors)
+FLAVORS=$(curl -s -H "X-Auth-Token: $TOKEN" ${ENDPOINT_URL}/${TENANT_ID}/flavors)
 N_FLAVORS=$(echo $FLAVORS |  grep -Po '"name":.*?[^\\]",'| wc -l)
 END=`date +%s`
 
--- scripts/nova/check_nova-instance.sh
+++ scripts/nova/check_nova-instance.sh
@@ -32,17 +32,18 @@ STATE_DEPENDENT=4
 usage ()
 {
     echo "Usage: $0 [OPTIONS]"
-    echo " -h               Get help"
-    echo " -H <Auth URL>    URL for obtaining an auth token. Ex: http://localhost"
-    echo " -T <tenant>      Tenant to use to get an auth token"
-    echo " -U <username>    Username to use to get an auth token"
-    echo " -P <password>    Password to use ro get an auth token"
-    echo " -N <server name> Name of the monitoring instance"
-    echo " -I <image name>  Name of the Glance image"
-    echo " -F <flavor name> Name of the Nova flavor"
+    echo " -h                   Get help"
+    echo " -H <Auth URL>        URL for obtaining an auth token. Ex: http://localhost:5000/v2.0"
+    echo " -E <Endpoint URL>    URL for nova API. Ex: http://localhost:8774/v2"
+    echo " -T <tenant>          Tenant to use to get an auth token"
+    echo " -U <username>        Username to use to get an auth token"
+    echo " -P <password>        Password to use ro get an auth token"
+    echo " -N <server name>     Name of the monitoring instance"
+    echo " -I <image name>      Name of the Glance image"
+    echo " -F <flavor name>     Name of the Nova flavor"
 }
 
-while getopts 'h:H:U:T:P:N:I:F:' OPTION
+while getopts 'hH:U:T:P:N:I:F:E:' OPTION
 do
     case $OPTION in
         h)
@@ -51,6 +52,9 @@ do
             ;;
         H)
             export OS_AUTH_URL=$OPTARG
+            ;;
+        E)
+            export ENDPOINT_URL=$OPTARG
             ;;
         T)
             export OS_TENANT=$OPTARG
@@ -77,6 +81,10 @@ do
     esac
 done
 
+# Set default values
+OS_AUTH_URL=${OS_AUTH_URL:-"http://localhost:5000/v2.0"}
+ENDPOINT_URL=${ENDPOINT_URL:-"http://localhost:8774/v2"}
+
 if ! which curl >/dev/null 2>&1
 then
     echo "curl is not installed."
@@ -85,13 +93,13 @@ fi
 
 ##### TOKEN PART #####
 # Get a token from Keystone
-TOKEN=$(curl -s -X 'POST' ${OS_AUTH_URL}:5000/v2.0/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'" ,"tenant":"'$OS_TENANT'"}}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TOKEN=$(curl -s -X 'POST' ${OS_AUTH_URL}/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'" ,"tenant":"'$OS_TENANT'"}}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 # Use the token to get a tenant ID. By default, it takes the second tenant
-TENANT_ID=$(curl -s -H "X-Auth-Token: $TOKEN" ${OS_AUTH_URL}:5000/v2.0/tenants |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep id|awk 'NR==1'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TENANT_ID=$(curl -s -H "X-Auth-Token: $TOKEN" ${OS_AUTH_URL}/tenants |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|grep id|awk 'NR==1'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 # Get a second token to avoid 401 error
-TOKEN_TENANT=$(curl -s -X 'POST' ${OS_AUTH_URL}:5000/v2.0/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'"} ,"tenantId":"'$TENANT_ID'"}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
+TOKEN_TENANT=$(curl -s -X 'POST' ${OS_AUTH_URL}/tokens -d '{"auth":{"passwordCredentials":{"username": "'$OS_USERNAME'", "password":"'$OS_PASSWORD'"} ,"tenantId":"'$TENANT_ID'"}}' -H 'Content-type: application/json' |sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}'|awk 'NR==2'|awk '{print $2}'|sed -n 's/.*"\([^"]*\)".*/\1/p')
 
 if [ -z "$TOKEN_TENANT" ]; then
     echo "Unable to get a token from Keystone API"
@@ -102,19 +110,19 @@ START=`date +%s`
 
 ##### INSTANCE PART #####
 # Get image ID
-IMAGE_ID=$(curl -s -i ${OS_AUTH_URL}:8774/v2/${TENANT_ID}/images -X GET -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,",") ; for (i=1; i<=n; i++) print a[i] }' | grep -B8 "$IMAGE_NAME" | awk -v var="id" '$0 ~ var { print $2 }' | sed 's/"//g')
+IMAGE_ID=$(curl -s -i ${ENDPOINT_URL}/${TENANT_ID}/images -X GET -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,",") ; for (i=1; i<=n; i++) print a[i] }' | grep -B8 "$IMAGE_NAME" | awk -v var="id" '$0 ~ var { print $2 }' | sed 's/"//g')
 
 # Get flavor ID
-FLAVOR_ID=$(curl -s -i ${OS_AUTH_URL}:8774/v2/${TENANT_ID}/flavors -X GET -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,",") ; for (i=1; i<=n; i++) print a[i] }' | grep -A1 "$FLAVOR_NAME" | awk -v var="id" '$0 ~ var { print $2 }' | sed 's/"//g')
+FLAVOR_ID=$(curl -s -i ${ENDPOINT_URL}/${TENANT_ID}/flavors -X GET -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,",") ; for (i=1; i<=n; i++) print a[i] }' | grep -A1 "$FLAVOR_NAME" | awk -v var="id" '$0 ~ var { print $2 }' | sed 's/"//g')
 
 # Spawn the new instance
-INSTANCE=$(curl -s -i ${OS_AUTH_URL}:8774/v2/${TENANT_ID}/servers -X POST -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" -d '{"server": {"name": "'$SERVER_NAME'", "imageRef": "'$IMAGE_ID'", "flavorRef": "'$FLAVOR_ID'", "max_count": 1, "min_count": 1, "networks": [], "security_groups": [{"name": "default"}]}}')
+INSTANCE=$(curl -s -i ${ENDPOINT_URL}/${TENANT_ID}/servers -X POST -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" -d '{"server": {"name": "'$SERVER_NAME'", "imageRef": "'$IMAGE_ID'", "flavorRef": "'$FLAVOR_ID'", "max_count": 1, "min_count": 1, "networks": [], "security_groups": [{"name": "default"}]}}')
 
 # Get the new instance ID
 INSTANCE_ID=$(echo $INSTANCE | awk  '{ n=split($0,a,",") ; for (i=1; i<=n; i++) print a[i] }' | awk -v var="id" '$0 ~ var { print $2 }' | sed 's/"//g')
 
 # Get the new instance status
-INSTANCE_STATUS=$(curl -s -i ${OS_AUTH_URL}:8774/v2/${TENANT_ID}/servers/${INSTANCE_ID} -X GET -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,"{") ; for (i=1; i<=n; i++) print a[i] }' | awk -v var="status" '$0 ~ var { print $2 }' | sed -e 's/"//g' -e 's/,//g')
+INSTANCE_STATUS=$(curl -s -i ${ENDPOINT_URL}/${TENANT_ID}/servers/${INSTANCE_ID} -X GET -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,"{") ; for (i=1; i<=n; i++) print a[i] }' | awk -v var="status" '$0 ~ var { print $2 }' | sed -e 's/"//g' -e 's/,//g')
 
 # While the instance is not in ACTIVE or ERROR state we check the status
 while [ "$INSTANCE_STATUS" != "ACTIVE"  ]
@@ -122,7 +130,7 @@ do
     sleep 5
    
     # Check the instance status 
-    INSTANCE_STATUS=$(curl -s -i ${OS_AUTH_URL}:8774/v2/${TENANT_ID}/servers/${INSTANCE_ID} -X GET -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,"{") ; for (i=1; i<=n; i++) print a[i] }' | awk -v var="status" '$0 ~ var { print $2 }' | sed -e 's/"//g' -e 's/,//g')
+    INSTANCE_STATUS=$(curl -s -i ${ENDPOINT_URL}/${TENANT_ID}/servers/${INSTANCE_ID} -X GET -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT" | awk '{ n=split($0,a,"{") ; for (i=1; i<=n; i++) print a[i] }' | awk -v var="status" '$0 ~ var { print $2 }' | sed -e 's/"//g' -e 's/,//g')
 
     if [ "$INSTANCE_STATUS" == "ERROR" ];
     then
@@ -134,7 +142,7 @@ END=`date +%s`
 TIME=$((END-START))
 
 # Delete the new instance
-curl -s ${OS_AUTH_URL}:8774/v2/${TENANT_ID}/servers/${INSTANCE_ID} -X DELETE -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT"
+curl -s ${ENDPOINT_URL}/${TENANT_ID}/servers/${INSTANCE_ID} -X DELETE -H "X-Auth-Project-Id: $OS_TENANT" -H "User-Agent: python-novaclient" -H "Accept: application/json" -H "X-Auth-Token: $TOKEN_TENANT"
 
 if [[ "$TIME" -gt "300" ]]; then
     echo "Unable to spawn instance."
