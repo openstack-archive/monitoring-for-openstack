b4eff1ddeac86ed63297b60ad2c7e3f64ce533f2
Frederic Lepied <frederic.lepied@enovance.com>
2014-08-01 16:37:19 +0200

    scripts/horizon/check_horizon_login.sh: rename ENDPOINT_URL to HORIZON_URL

--- scripts/horizon/check_horizon_login.sh
+++ scripts/horizon/check_horizon_login.sh
@@ -42,12 +42,12 @@ usage ()
 }
 
 output_result () {
-	# Output check result & refresh cache if requested
-	MSG="$1"
-	RETCODE=$2
+    # Output check result & refresh cache if requested
+    MSG="$1"
+    RETCODE=$2
 	
-	echo "$MSG"
-	exit $RETCODE
+    echo "$MSG"
+    exit $RETCODE
 }
 
 while getopts 'hH:U:P:E:c' OPTION
@@ -58,7 +58,7 @@ do
             exit 0
             ;;
         E)
-            export ENDPOINT_URL=$OPTARG
+            export HORIZON_URL=$OPTARG
             ;;
         U)
             export OS_USERNAME=$OPTARG
@@ -78,7 +78,7 @@ done
 
 COOKIE_FILE=${COOKIE_FILE:-"/tmp/check_horizon_cookieFile"}
 
-if [ -z "$OS_USERNAME" ] || [ -z "$OS_PASSWORD" ] || [ -z "$ENDPOINT_URL" ] 
+if [ -z "$OS_USERNAME" ] || [ -z "$OS_PASSWORD" ] || [ -z "$HORIZON_URL" ] 
 then
     usage
     exit 1
@@ -86,29 +86,29 @@ fi
 
 if ! which curl > /dev/null 2>&1
 then
-	output_result "UNKNOWN - curl is not installed." $STATE_UNKNOWN
+    output_result "UNKNOWN - curl is not installed." $STATE_UNKNOWN
 fi
 
 
 # Get CSRFTOKEN and REGION on index
-GET_INDEX=`curl -c $COOKIE_FILE -i $ENDPOINT_URL 2> /dev/null`
+GET_INDEX=$(curl -s -c $COOKIE_FILE -i $HORIZON_URL)
 
 if [ -z "$GET_INDEX" ]
 then
-    output_result "CRITICAL - $ENDPOINT_URL not respond." $STATE_CRITICAL
+    output_result "CRITICAL - $HORIZON_URL not respond." $STATE_CRITICAL
 fi
 
 CSRFTOKEN=$(echo $GET_INDEX | sed -r "s/.*csrfmiddlewaretoken['\"] value=['\"]([^'\"]+)['\"].*/\1/")
 REGION=$(echo $GET_INDEX | sed -r "s/.*region['\"] value=['\"]([^\"']+)['\"].*/\1/")
 
 # Send POST login with CSRFTOKEN and REGION on /auth/login/
-RESULT=`curl -L -b "$COOKIE_FILE" --referer $ENDPOINT_URL --data "username=$OS_USERNAME&password=$OS_PASSWORD&region=$REGION&csrfmiddlewaretoken=$CSRFTOKEN" $ENDPOINT_URL/auth/login/ 2> /dev/null`
+RESULT=$(curl -s -L -b "$COOKIE_FILE" --referer $HORIZON_URL --data "username=$OS_USERNAME&password=$OS_PASSWORD&region=$REGION&csrfmiddlewaretoken=$CSRFTOKEN" $HORIZON_URL/auth/login/)
 
 
 # If Auth work, find patterns Overview
 if [[ $RESULT == *Overview* ]]
 then
-    output_result "OK - Find string Overview in $ENDPOINT_URL" $STATE_OK
+    output_result "OK - Found string Overview in $HORIZON_URL" $STATE_OK
 else
-    output_result "CRITICAL - String \"Overview\" not found in $ENDPOINT_URL" $STATE_OK
+    output_result "CRITICAL - String \"Overview\" not found in $HORIZON_URL" $STATE_OK
 fi
