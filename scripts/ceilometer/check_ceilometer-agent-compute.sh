81a0f7e8d7b7eed0045b997b87937e9e9473be6e
Julien Danjou <julien@danjou.info>
2014-08-01 15:28:34 +0200

    Fix typo in variable name

--- scripts/ceilometer/check_ceilometer-agent-compute.sh
+++ scripts/ceilometer/check_ceilometer-agent-compute.sh
@@ -26,7 +26,7 @@ STATE_OK=0
 STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
-DEAMON='ceilometer-agent-compute'
+DAEMON='ceilometer-agent-compute'
 STATE_DEPENDENT=4
 
 usage ()
@@ -59,24 +59,24 @@ fi
 
 
 
-PID=$(pidof -x $DEAMON || true)
+PID=$(pidof -x $DAEMON || true)
 if [ -z $PID ]; then
-    echo "$DEAMON is not running."
+    echo "$DAEMON is not running."
     exit $STATE_CRITICAL
 fi
 
 if [ "$(id -u)" != "0" ]; then
-    echo "$DEAMON is running but the script must be run as root"
+    echo "$DAEMON is running but the script must be run as root"
     exit $STATE_WARNING
 else
 
     #Need root to "run netstat -p"
     if ! KEY=$(netstat -epta 2>/dev/null | awk "{if (/amqp.*${PID}\/python/) {print ; exit}}") || test -z "$KEY"
     then
-        echo "$DEAMON is not connected to AMQP"
+        echo "$DAEMON is not connected to AMQP"
         exit $STATE_CRITICAL
     fi
 fi
 
-echo "$DEAMON is working."
+echo "$DAEMON is working."
 exit $STATE_OK
